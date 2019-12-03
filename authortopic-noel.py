345cf5019e46a140fc4794f67b715621a25e4059
nwarford <noel.warford95@gmail.com>
2019-12-03 11:21:43 -0500

    got rid of the evidence

--- authortopic-noel.py
+++ authortopic-noel.py
@@ -25,35 +25,6 @@ author2doc = {
     'author2': author2
 }
 
-# corpus = mmcorpus.MmCorpus(datapath('testcorpus.mm'))
-
-# First, let's make an array that has 5 entries with 96 potential columns
-# basically, we're doing doc2bow manually
-# bag_of_words = []
-# for i in range(5) :
-#     # pick 10 mutations for each genome
-#     topic_dict = {}
-#     for k in range(10) :
-#         mutation = random.randint(0,95)
-#         if mutation not in topic_dict :
-#             topic_dict[mutation] = 1
-#         else :
-#             currval = topic_dict[mutation]
-#             topic_dict[mutation] = currval + 1
-#
-#     # make them tuples, like bag of words in gensim expects
-#     topic_list = []
-#     for entry in topic_dict :
-#         tuple = (entry, float(topic_dict[entry]))
-#         topic_list.append(tuple)
-#
-#     bag_of_words.append(topic_list)
-#
-# # bag_of_words is now a bag-of-words-style corpus in the way gensim expects
-# # so now we can generate a dictionary straight from the bag of words, rather than the normal streaming order
-# # from https://radimrehurek.com/gensim/corpora/dictionary.html#gensim.corpora.dictionary.Dictionary.from_corpus
-# mutation_dict = Dictionary.from_corpus(bag_of_words)
-
 data = np.random.randint(low = 0, high = 100, size = (96, 6), dtype = int)
 for i in range(48):
     for j in range(3):
