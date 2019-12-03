50bf9f69d8daa759c6361f3ae622e46ea75e3d30
mknittel <mknittel@g.hmc.edu>
2019-12-02 19:17:38 -0500

    Updated lda

--- lda_mutations.py
+++ lda_mutations.py
@@ -4,37 +4,59 @@ from gensim.matutils import Scipy2Corpus
 from scipy import sparse
 from gensim.corpora import Dictionary
 
-data = np.random.randint(low = 0, high = 100, size = (96, 5), dtype = int)
-#print(data)
-corpus = sparse.csc_matrix(data)
-print(corpus)
-num_topics = 96
-chunksize = 20
-passes = 20
-iterations = 400
-eval_every = None
+from gensim.test.utils import common_texts
+
+# Create a corpus from a list of texts
+#common_dictionary = Dictionary(common_texts)
+#common_corpus = [common_dictionary.doc2bow(text) for text in common_texts]
+#print(common_texts)
+
+data = np.random.randint(low = 0, high = 100, size = (96, 6), dtype = int)
+for i in range(48):
+    for j in range(3):
+        data[i,j] = 0
+        data[95-i,5-j] = 0
+data = data.T
+print(data)
 
 dictionary = Dictionary(data)
-print(dictionary)
-temp = dictionary[0]  # This is only to "load" the dictionary.                                           
-id2word = dictionary.id2token
+
+corpus = [dictionary.doc2bow(text) for text in data]
+#print(data)
+#corpus = sparse.csc_matrix(data)
+# print(corpus)
+num_topics = 2
+chunksize = 2000
+passes = 1
+iterations = 50
+eval_every = 10
+
+#dictionary = Dictionary(data)
+#print(dictionary)
+#temp = dictionary[0]  # This is only to "load" the dictionary.                                           
+#id2word = dictionary.id2token
+#id2word = common_dictionary
+id2word = dictionary
 
 model = LdaModel(
     corpus=corpus,
-    id2word=id2word
-    #chunksize=chunksize,
+    num_topics=num_topics,
+    id2word=id2word,
+    chunksize=chunksize,
+    passes=passes,
+    iterations=iterations,
+    eval_every=eval_every
     #alpha='auto',
     #eta='auto',
-    #iterations=iterations,
-    #num_topics=num_topics,
-    #passes=passes,
-    #eval_every=eval_every
 )
 
 top_topics = model.top_topics(corpus)
+print(top_topics)
+print(type(top_topics))
 
-#avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
-#print('Average topic coherence: %.4f.' % avg_topic_coherence)
+avg_topic_coherence = sum([t[1] for t in top_topics]) / num_topics
+print('Average topic coherence: %.4f.' % avg_topic_coherence)
+tops = model.get_topics()
 
 #from pprint import pprint
 #pprint(top_topics)
