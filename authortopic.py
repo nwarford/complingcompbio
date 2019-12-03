9719f255d9aab51bdabc21044b170c7d7290dad8
kfulton121 <kfulton121@gmail.com>
2019-12-03 14:00:27 -0500

    Trying to use real data

--- authortopic.py
+++ authortopic.py
@@ -3,7 +3,7 @@ from gensim.corpora import mmcorpus, Dictionary
 # from gensim.test.utils import common_dictionary, datapath, temporary_file
 import random
 import numpy as np
-
+import pandas as pd
 # Adapted by Noel from the example at https://radimrehurek.com/gensim/models/atmodel.html
 
 # generate authorship based on a simple rule - author0: 0, 3, 6; author1: 1, 4, 7; author2: 2, 5, 8
@@ -25,13 +25,16 @@ author2doc = {
     'author2': author2
 }
 
-data = np.random.randint(low = 0, high = 100, size = (96, 6), dtype = int)
-for i in range(48):
-    for j in range(3):
-        data[i,j] = 0
-        data[95-i,5-j] = 0
-data = data.T
-print(data)
+#data = np.random.randint(low = 0, high = 100, size = (96, 6), dtype = int)
+#for i in range(48):
+    #for j in range(3):
+        #data[i,j] = 0
+        #data[95-i,5-j] = 0
+#data = data.T
+#print(data)
+df = pd.read_csv("sbs_counts.tsv", sep="\t", index_col = 0)
+
+data = df.to_numpy(dtype = int)
 
 dictionary = Dictionary(data)
 
